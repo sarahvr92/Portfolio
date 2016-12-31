3cf02af213e710c9d24755855f17a9faa78762ab
Sarah <sarahvr92@gmail.com>
2016-12-31 18:23:21 -0500

    Update default page layout

--- page.php
+++ page.php
@@ -1,23 +1,22 @@
 <?php get_header(); ?>
-	
-	<div id="content">
-	
-		<div id="inner-content" class="row">
-	
-		    <main id="main" class="large-8 medium-8 columns" role="main">
-				
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div id="content">
+        <div id="inner-content" class="row">
+            <main id="main" class="small-12 column" role="main">
+                <?php if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
+                            <header class="article-header">
+                                <h1 class="page-title"><?php the_title(); ?></h1>
+                            </header>
 
-			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
-			    
-			    <?php endwhile; endif; ?>							
-			    					
-			</main> <!-- end #main -->
-
-		    <?php get_sidebar(); ?>
-		    
-		</div> <!-- end #inner-content -->
-
-	</div> <!-- end #content -->
-
+                            <section class="entry-content" itemprop="articleBody">
+                                <?php the_content(); ?>
+                                <?php wp_link_pages(); ?>
+                            </section>
+                        </article>
+                    <?php endwhile;
+                endif; ?>
+            </main> <!-- end #main -->
+        </div> <!-- end #inner-content -->
+    </div> <!-- end #content -->
 <?php get_footer(); ?>
