18a3b4758a7497f7dc04f03cf324f7af82d1865b
Sarah <sarahvr92@gmail.com>
2016-12-31 14:38:11 -0500

    Simplify page template part

--- parts/loop-page.php
+++ parts/loop-page.php
@@ -1,18 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-						
-	<header class="article-header">
-		<h1 class="page-title"><?php the_title(); ?></h1>
-	</header> <!-- end article header -->
-					
+    <header class="article-header">
+        <h1 class="page-title"><?php the_title(); ?></h1>
+    </header>
+
     <section class="entry-content" itemprop="articleBody">
-	    <?php the_content(); ?>
-	    <?php wp_link_pages(); ?>
-	</section> <!-- end article section -->
-						
-	<footer class="article-footer">
-		
-	</footer> <!-- end article footer -->
-						    
-	<?php comments_template(); ?>
-					
-</article> <!-- end article -->
+        <?php the_content(); ?>
+        <?php wp_link_pages(); ?>
+    </section>
+</article>
