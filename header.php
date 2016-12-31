bf372969f81896f7e6221b6c19a883621f231c0e
Sarah <sarahvr92@gmail.com>
2016-12-31 17:38:26 -0500

    Update header, add GTM code

--- header.php
+++ header.php
@@ -1,6 +1,21 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
+    <script>
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NF2WST');
+    </script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,10 +28,18 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div class="off-canvas-wrapper">
-    <?php get_template_part('parts/content', 'offcanvas'); ?>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NF2WST" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 
+<div class="off-canvas-wrapper">
     <div class="off-canvas-content" data-off-canvas-content>
         <header class="header" role="banner">
-            <?php get_template_part('parts/nav', 'offcanvas-topbar'); ?>
+            <div class="header-secondary">
+                <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+            </div>
+
+            <div class="header-primary">
+                <?php joints_off_canvas_nav(); ?>
+            </div>
         </header>
