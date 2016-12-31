d816b8e9ff17f56d33de38eada343965b1658bd3
Sarah <sarahvr92@gmail.com>
2016-12-31 12:47:20 -0500

    Set theme colour to be white

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta class="foundation-mq">
-    <meta name="theme-color" content="#121212">
+    <meta name="theme-color" content="#FFFFFF">
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
