efa825336371fbfb6b9076b3e0787317547d019f
Sarah <sarahvr92@gmail.com>
2016-12-31 14:26:00 -0500

    Begin to update menus

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
 <footer class="footer" role="contentinfo">
     <div id="inner-footer" class="row">
-        <div class="large-12 medium-12 columns">
-            <nav role="navigation">
-                <?php joints_footer_links(); ?>
-            </nav>
-        </div>
-        <div class="large-12 medium-12 columns">
+        <div class="column">
             <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
         </div>
     </div>
