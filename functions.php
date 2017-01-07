8e67adc10bb340b4020d29f1e6c4434c42808139
Sarah <sarahvr92@gmail.com>
2017-01-07 16:43:03 -0500

    Move menu functions, delete custom post type files

--- functions.php
+++ functions.php
@@ -5,17 +5,14 @@ require_once(get_template_directory() . '/functions/enqueue-scripts.php');
 require_once(get_template_directory() . '/functions/admin.php');
 require_once(get_template_directory() . '/functions/login.php');
 require_once(get_template_directory() . '/functions/theme-options.php');
+require_once(get_template_directory() . '/functions/menu.php');
 
-require_once(get_template_directory() . '/assets/functions/menu.php');
 require_once(get_template_directory() . '/assets/functions/sidebar.php');
 require_once(get_template_directory() . '/assets/functions/comments.php');
 require_once(get_template_directory() . '/assets/functions/page-navi.php');
 
 // Remove 4.2 Emoji Support
 // require_once(get_template_directory().'/assets/functions/disable-emoji.php');
-
-// Use this as a template for custom post types
-// require_once(get_template_directory().'/assets/functions/custom-post-type.php');
 
 // Remove All Yoast HTML Comments
 if (defined('WPSEO_VERSION')) {
