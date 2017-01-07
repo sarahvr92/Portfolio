4c565c534da1c3ce76ba55d1c18178031626ac7d
Sarah <sarahvr92@gmail.com>
2017-01-07 18:50:09 -0500

    Add front-page template

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+    <div id="content">
+        <div id="inner-content" class="row align-center">
+            <main id="main" class="small-10 column" role="main">
+                <?php if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
+                            <header class="article-header">
+                                <h1 class="page-title"><?php the_title(); ?></h1>
+                            </header>
+
+                            <section class="entry-content" itemprop="articleBody">
+                                <?php the_content(); ?>
+                                <?php wp_link_pages(); ?>
+                            </section>
+                        </article>
+                    <?php endwhile;
+                endif; ?>
+            </main>
+        </div>
+    </div>
+<?php get_footer(); ?>
