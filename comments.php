09ffe334f814f93e527db61f5eb28d385ef6e609
Sarah <sarahvr92@gmail.com>
2017-01-07 18:34:08 -0500

    Remove search and cleanup comments

--- comments.php
+++ comments.php
@@ -1,61 +1,49 @@
-<?php
-	if ( post_password_required() ) {
-		return;
-	}
-?>
+<?php if (post_password_required()) {
+    return;
+} ?>
 
 <div id="comments" class="comments-area">
+    <?php if (have_comments()) : ?>
+        <h2 class="comments-title">
+            <?php printf( // WPCS: XSS OK.
+                esc_html(_nx('One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title')),
+                number_format_i18n(get_comments_number()),
+                '<span>' . get_the_title() . '</span>'
+            ); ?>
+        </h2>
 
-	<?php // You can start editing here ?>
+        <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+            <nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php _e('Comment navigation'); ?></h2>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'jointswp' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
+                <div class="nav-links">
+                    <div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments')); ?></div>
+                    <div class="nav-next"><?php next_comments_link(esc_html__('Newer Comments')); ?></div>
+                </div>
+            </nav>
+        <?php endif; ?>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'jointswp' ); ?></h2>
-			<div class="nav-links">
+        <ol class="commentlist">
+            <?php wp_list_comments('type=comment&callback=joints_comments'); ?>
+        </ol>
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'jointswp' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'jointswp' ) ); ?></div>
+        <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+            <nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php _e('Comment navigation'); ?></h2>
+                <div class="nav-links">
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+                    <div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments')); ?></div>
+                    <div class="nav-next"><?php next_comments_link(esc_html__('Newer Comments')); ?></div>
 
-		<ol class="commentlist">
-			<?php wp_list_comments('type=comment&callback=joints_comments'); ?>
-		</ol>
+                </div>
+            </nav>
+        <?php endif; ?>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'jointswp' ); ?></h2>
-			<div class="nav-links">
+    <?php endif; ?>
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'jointswp' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'jointswp' ) ); ?></div>
+    <?php if (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
+        <p class="no-comments"><?php _e('Comments are closed.'); ?></p>
+    <?php endif; ?>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // Check for comment navigation. ?>
-
-	<?php endif; // Check for have_comments(). ?>
-
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'jointswp' ); ?></p>
-	<?php endif; ?>
-
-	<?php comment_form(array('class_submit'=>'button')); ?>
-
-</div><!-- #comments -->
+    <?php comment_form(array('class_submit' => 'button')); ?>
+</div>
