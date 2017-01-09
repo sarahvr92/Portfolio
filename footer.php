6922a922148338f0101f7a6ba0592a3c49da2a69
Sarah <sarahvr92@gmail.com>
2017-01-08 19:37:43 -0500

    Add mobile only footer

--- footer.php
+++ footer.php
@@ -1,3 +1,36 @@
+<footer class="footer text-center hide-for-medium">
+    <ul class="social-media-list">
+        <?php
+        $social_media = array(
+            'dribbble' => array(
+                'url' => portfolio_get_option('_theme_dribbble'),
+                'img' => get_template_directory() . '/assets/images/dribbble.svg'
+            ),
+            'github'   => array(
+                'url' => portfolio_get_option('_theme_github'),
+                'img' => get_template_directory() . '/assets/images/github.svg'
+            ),
+            'linkedin' => array(
+                'url' => portfolio_get_option('_theme_linkedin'),
+                'img' => get_template_directory() . '/assets/images/linkedin.svg'
+            ),
+            'twitter'  => array(
+                'url' => portfolio_get_option('_theme_twitter'),
+                'img' => get_template_directory() . '/assets/images/twitter.svg'
+            ),
+        );
+
+        foreach ($social_media as $social_profile):
+            if ($social_profile['url']): ?>
+                <li>
+                    <a href="<?php echo $social_profile['url']; ?>" target="_blank">
+                        <?php echo file_get_contents($social_profile['img']); ?>
+                    </a>
+                </li>
+            <?php endif;
+        endforeach; ?>
+    </ul>
+</footer>
 </div>
 </div>
 <?php wp_footer(); ?>
