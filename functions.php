75d1dd0adf69a1dbc8f88a5cafb13f41ccf38224
Sarah <sarahvr92@gmail.com>
2017-03-14 22:33:54 -0400

    Disable emoji support until blog integration

--- functions.php
+++ functions.php
@@ -7,13 +7,11 @@ require_once(get_template_directory() . '/functions/login.php');
 require_once(get_template_directory() . '/functions/theme-options.php');
 require_once(get_template_directory() . '/functions/menu.php');
 require_once(get_template_directory() . '/functions/metaboxes.php');
+require_once(get_template_directory() . '/functions/disable-emoji.php');
 
 require_once(get_template_directory() . '/assets/functions/sidebar.php');
 require_once(get_template_directory() . '/assets/functions/comments.php');
 require_once(get_template_directory() . '/assets/functions/page-navi.php');
-
-// Remove 4.2 Emoji Support
-// require_once(get_template_directory().'/assets/functions/disable-emoji.php');
 
 add_filter('post_thumbnail_html', 'remove_width_attribute', 10);
 add_filter('image_send_to_editor', 'remove_width_attribute', 10);
