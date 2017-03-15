4aa09295b536aa95e693f7d1637196e393f448d1
Sarah <sarahvr92@gmail.com>
2017-03-14 22:20:07 -0400

    Style Split Page

--- front-page.php
+++ front-page.php
@@ -1,18 +1,31 @@
 <?php get_header(); ?>
     <div id="content">
-        <div id="inner-content" class="row align-center">
-            <main id="main" class="small-10 column" role="main">
+        <div id="inner-content">
+            <main id="main" role="main" class="split-page">
                 <?php if (have_posts()) :
                     while (have_posts()) : the_post(); ?>
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-                            <header class="article-header">
-                                <h1 class="page-title"><?php the_title(); ?></h1>
-                            </header>
+                        <div class="split-page-image-container">
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
 
-                            <section class="entry-content" itemprop="articleBody">
-                                <?php the_content(); ?>
-                                <?php wp_link_pages(); ?>
-                            </section>
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('row split-page-row'); ?> role="article"
+                                 itemscope itemtype="http://schema.org/WebPage">
+                            <div class="small-10 large-8 xlarge-6 column">
+                                <header class="split-page-content-header">
+                                    <h1>
+                                        <?php if ($subtitle = trim(get_post_meta(get_the_ID(), '_split_page_subtitle', true))): ?>
+                                            <span class="subheader"><?php echo $subtitle; ?></span>
+                                        <?php endif; ?>
+
+                                        <?php echo trim(get_post_meta(get_the_ID(), '_split_page_title', true)) ?: get_the_title(); ?>
+                                    </h1>
+                                </header>
+
+                                <section class="entry-content split-page-content" itemprop="articleBody">
+                                    <?php the_content(); ?>
+                                    <?php wp_link_pages(); ?>
+                                </section>
+                            </div>
                         </article>
                     <?php endwhile;
                 endif; ?>
