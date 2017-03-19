e7dc8fbba3c0a69a0d5aa1e4845a9ce7f1614d2f
Sarah <sarahvr92@gmail.com>
2017-03-19 15:08:17 -0400

    Complete MVP styles

--- front-page.php
+++ front-page.php
@@ -8,23 +8,30 @@
                             <?php the_post_thumbnail('large'); ?>
                         </div>
 
-                        <article id="post-<?php the_ID(); ?>" <?php post_class('row split-page-row'); ?> itemscope
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('split-page-content-container'); ?> itemscope
                                  itemtype="http://schema.org/WebPage">
-                            <div class="small-10 large-8 xlarge-6 column">
-                                <header class="split-page-content-header">
-                                    <h1 class="entry-title">
-                                        <?php if ($subtitle = trim(get_post_meta(get_the_ID(), '_split_page_subtitle', true))): ?>
-                                            <span class="subheader"><?php echo $subtitle; ?></span>
-                                        <?php endif; ?>
+                            <div class="row split-page-content-header-row">
+                                <div class="small-1 medium-2 column hide-for-large"></div>
+                                <div class="small-10 medium-9 large-10 column">
+                                    <header class="split-page-content-header">
+                                        <h1 class="entry-title">
+                                            <?php if ($subtitle = trim(get_post_meta(get_the_ID(), '_split_page_subtitle', true))): ?>
+                                                <span class="subheader"><?php echo $subtitle; ?></span>
+                                            <?php endif; ?>
 
-                                        <?php echo trim(get_post_meta(get_the_ID(), '_split_page_title', true)) ?: get_the_title(); ?>
-                                    </h1>
-                                </header>
+                                            <?php echo trim(get_post_meta(get_the_ID(), '_split_page_title', true)) ?: get_the_title(); ?>
+                                        </h1>
+                                    </header>
+                                </div>
+                            </div>
 
-                                <section class="entry-content split-page-content">
-                                    <?php the_content(); ?>
-                                    <?php wp_link_pages(); ?>
-                                </section>
+                            <div class="row split-page-content-row">
+                                <div class="small-10 medium-8 large-10 column">
+                                    <section class="entry-content split-page-content">
+                                        <?php the_content(); ?>
+                                        <?php wp_link_pages(); ?>
+                                    </section>
+                                </div>
                             </div>
                         </article>
                     <?php endwhile;
